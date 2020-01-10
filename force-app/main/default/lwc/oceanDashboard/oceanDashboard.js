e8f7675a1bd0a8ea1bb8fdb77b86188324cae685
Shintu-J-Thattakunnil <48328907+Shintu-J-Thattakunnil@users.noreply.github.com>
2020-01-10 11:39:07 -0500

    Home - Wave updates

--- force-app/main/default/lwc/oceanDashboard/oceanDashboard.js
+++ force-app/main/default/lwc/oceanDashboard/oceanDashboard.js
@@ -1,5 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 import getCurrentOceanWave from "@salesforce/apex/OceanController.getCurrentOceanWave";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class oceanDashboard extends LightningElement {
+  @track currWaveDueDate;
+  @track nextWaveDueDate;
+
+  connectedCallback() {
+      this.getWaveDetails();
+  }
+
+  getWaveDetails() {
+    getCurrentOceanWave()
+      .then((result) => {
+      
+        if (result) {
+          this.currWaveDueDate = result[0]
+            ? result[0].ADO_Submission_Due_Date__c
+            : undefined;
+          this.nextWaveDueDate = result[1]
+            ? result[1].ADO_Submission_Due_Date__c
+            : undefined;
+        }
+      })
+      .catch(error => {
+        this.dispatchEvent(
+            new ShowToastEvent({
+              title: "Error on querying wave details",
+              message: error.message,
+              variant: "error"
+            })
+          );
+      });
+  }
 }
