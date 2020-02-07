4bfe6aa571b511c2ca109d5c357148b5fafb35b5
subha-radiantt <57965748+subha-radiantt@users.noreply.github.com>
2020-02-07 12:53:48 -0500

    Recent Changes

--- force-app/main/default/lwc/oceanDashboard/oceanDashboard.js
+++ force-app/main/default/lwc/oceanDashboard/oceanDashboard.js
@@ -7,19 +7,19 @@ export default class oceanDashboard extends LightningElement {
   @track nextWaveDueDate;
 
   connectedCallback() {
-      this.getWaveDetails();
+    this.getWaveDetails();
   }
 
-  getFormattedDate(dateString){
-    const dateArr = dateString.split('-');
-    const date = new Date(dateArr[0], dateArr[1]-1, dateArr[2]);
-    return new Intl.DateTimeFormat('en-US').format(date);
+  getFormattedDate(dateString) {
+    const dateArr = dateString.split("-");
+    const date = new Date(dateArr[0], dateArr[1] - 1, dateArr[2]);
+    return new Intl.DateTimeFormat("en-US").format(date);
   }
 
   getWaveDetails() {
     getCurrentOceanWave()
-      .then((result) => {
-        if (result) {          
+      .then(result => {
+        if (result) {
           this.currWaveDueDate = result[0]
             ? this.getFormattedDate(result[0].ADO_Submission_Due_Date__c)
             : undefined;
@@ -30,12 +30,12 @@ export default class oceanDashboard extends LightningElement {
       })
       .catch(error => {
         this.dispatchEvent(
-            new ShowToastEvent({
-              title: "Error on querying wave details",
-              message: error.message,
-              variant: "error"
-            })
-          );
+          new ShowToastEvent({
+            title: "Error on querying wave details",
+            message: error.message,
+            variant: "error"
+          })
+        );
       });
   }
 }
